8d900e955e4bf745ccba769f573b531c1ab2b45c
akumar-cs <akumar@cognitivescale.com>
2019-12-18 19:22:59 +0530

    static split wip

--- bank_marketing/decisionTree/train.py
+++ bank_marketing/decisionTree/train.py
@@ -6,30 +6,42 @@ import sys
 from bank_marketing.common_utils.train_utils import Encoder
 from utils.encode_decode import pickle_model
 
+
 def train(msg):
-    random.seed(0)
     training_data_uri = msg.payload.get("$ref", "./data/bank_marketing-prepped.csv")
     save_model_as = msg.payload.get("model_name")
+
     data = pd.read_csv(training_data_uri)
-    feature_names = list(data.columns[:-1])
+    train_dataset = training_data_uri.replace(".csv", "-train.csv")
+    test_dataset = training_data_uri.replace(".csv", "-test.csv")
+    train_data = pd.read_csv(train_dataset)
+    test_data = pd.read_csv(test_dataset)
 
     # Separate outcome
     y = data["deposit"]
     X = data.drop("deposit", axis=1)
 
-    # apply encoder data
+    y_train_df = train_data["deposit"]
+    X_train_df = train_data.drop("deposit", axis=1)
+
+    y_test_df = test_dataset["deposit"]
+    X_test_df = test_dataset.drop("deposit", axis=1)
+
+    # create encoder on entire dataset
     scaler = Encoder(X)
     scaler.fit(X)
 
-    X_onehot = scaler.transform(X)
+    # apply encoding to train and test data features
+    X_train = scaler.transform(X_train_df)
+    y_train = y_train_df
 
-    # split test and training
-    X_train, X_test, y_train, y_test = train_test_split(
-        X_onehot, y, test_size=0.3, random_state=4
-    )
+    X_test = scaler.transform(X_test_df)
+    y_test = y_test_df
+
     # start model training
     dtree = DecisionTreeClassifier(criterion="entropy", random_state=0)
     dtree.fit(X_train.values, y_train.values)
+
     dtree_acc = dtree.score(X_test.values, y_test.values)
     model_binary = f"models/{save_model_as}.pkl"
     pickle_model(
