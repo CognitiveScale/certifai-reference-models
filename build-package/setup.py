a07d1bcf95ae9e92d76eca946d19d6404f660265
akumar-cs <akumar@cognitivescale.com>
2020-01-29 09:55:30 -0600

    bump tensorflow version to 1.15.2

--- build-package/setup.py
+++ build-package/setup.py
@@ -36,7 +36,7 @@ setup(name='cortex-certifai-reference-model-server',
         'Flask==1.1.1',
         'cortex-python==1.1.0',
         'keras==2.3.1',
-        'tensorflow==1.15.0',
+        'tensorflow==1.15.2',
       ],
 
 
