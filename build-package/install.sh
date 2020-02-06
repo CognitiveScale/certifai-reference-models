a77748f2ba1525165060e7dfa4bfaa9940bc69a0
akumar-cs <akumar@cognitivescale.com>
2020-02-06 12:04:09 -0600

    updated install with versioning

--- build-package/install.sh
+++ build-package/install.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
+CFI_CURRENT_VERSION_LONG=$(git describe --long --always --match='v*.*' | sed 's/v//')
+CFI_CURRENT_VERSION=$(echo ${CFI_CURRENT_VERSION_LONG} | cut -d '-' -f 1)
+CFI_VERSION_BUILD_NUM=$(echo ${CFI_CURRENT_VERSION_LONG} | cut -d '-' -f 2-)
+CFI_PKG_VERSION=$(echo ${CFI_CURRENT_VERSION} | awk -F. -v OFS=. -v f=3 '{ $$f++ } 1')
+
+echo ${CFI_PKG_VERSION} ${CFI_CURRENT_VERSION_LONG} ${CFI_VERSION_BUILD_NUM}
+sed -i '' "s/__version__ = '.*'/__version__ = '${CFI_PKG_VERSION}'/" setup.py
+
 # create package to register package namespace
 python setup.py sdist  --format=zip
-pip install dist/cortex-certifai-reference-model-server-0.5.0.zip
 
-# run train with installed package
+mv dist/cortex-certifai-reference-model-server-${CFI_PKG_VERSION}.zip dist/cortex-certifai-reference-model-server-${CFI_PKG_VERSION}-${CFI_VERSION_BUILD_NUM}.zip
+
+pip install dist/cortex-certifai-reference-model-server-${CFI_PKG_VERSION}-${CFI_VERSION_BUILD_NUM}.zip
+
+run train with installed package
 cd certifaiReferenceModelServer/all && make train-projects-all-local
 
 # mv models from projects and build-again
 
 cd ../ && mkdir -p models/ && rm -rf models/* && find . -type f -name '*.pkl' -exec mv {} models/ \;
 
-cd .. && python setup.py sdist  --format=zip && pip install dist/cortex-certifai-reference-model-server-0.5.0.zip
+cd .. && python setup.py sdist  --format=zip && mv dist/cortex-certifai-reference-model-server-${CFI_PKG_VERSION}.zip dist/cortex-certifai-reference-model-server-${CFI_PKG_VERSION}-${CFI_VERSION_BUILD_NUM}.zip && pip install dist/cortex-certifai-reference-model-server-${CFI_PKG_VERSION}-${CFI_VERSION_BUILD_NUM}.zip
